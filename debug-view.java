973aa778c9e1ee42cd6d4f92bc51bef3ad07af96
黒狗さん。 <68948165+pochi1111@users.noreply.github.com>
2023-01-07 09:35:29 +0900

    Update debug-view.java

--- debug-view.java
+++ debug-view.java
@@ -13,24 +13,17 @@ import static test.official_book.official_book.Official_book.plugin;
 public class Listeners implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event){
-        Set<String> UserTags = event.getWhoClicked().getScoreboardTags();
-        if(UserTags.contains("Official_Book_Open")){
-//            String item_name;
-//            item_name = event.getCursor().getItemMeta().getDisplayName();
-//            if(plugin.getConfig().getStringList("books.names").contains(item_name)){
-//                Player e = (Player) event.getWhoClicked();
-//                e.sendMessage("本を開きます");
-//                e.closeInventory();
-//                e.performCommand("official_b "+item_name);
-//                e.getPlayer().removeScoreboardTag("Official_Book_Open");
-//            }
+        if(event.getView().getTitle().equals("公式本")){
+            String item_name;
             event.setCancelled(true);
-        }
-    }
-    public void onInventoryClose(InventoryCloseEvent event){
-        Set<String> UserTags = event.getPlayer().getScoreboardTags();
-        if(UserTags.contains("Official_Book_Open")){
-            event.getPlayer().removeScoreboardTag("Official_Book_Open");
+            item_name = event.getCurrentItem().getItemMeta().getDisplayName();
+            if(plugin.getConfig().getStringList("books.names").contains(item_name)){
+                Player e = (Player) event.getWhoClicked();
+                e.sendMessage("本を開きます");
+                e.closeInventory();
+                //ここでバグ
+                e.performCommand("official_b "+item_name);
+            }
         }
     }
 }
